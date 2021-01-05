513ff3fef80df2ae855c5a8ff9c4327a0f8831b7
unkn0wn_l <ap.qwert@yandex.ru>
2021-01-05 14:41:49 +0300

    init commmit

---
+++ src/main/java/com/innowise/coffeemachine/CoffeeMachineApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.coffeemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innowise/coffeemachine/CoffeeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.coffeemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
