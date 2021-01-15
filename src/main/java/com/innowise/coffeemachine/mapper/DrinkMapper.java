392edd9851fa0e812b7bb927155cfa344a76f749
unkn0wn_l <ap.qwert@yandex.ru>
2021-01-15 11:48:06 +0300

    mappers was refactored for spring cdi container

--- src/main/java/com/innowise/coffeemachine/mapper/DrinkMapper.java
+++ src/main/java/com/innowise/coffeemachine/mapper/DrinkMapper.java
@@ -3,12 +3,9 @@ package com.innowise.coffeemachine.mapper;
 import com.innowise.coffeemachine.dto.DrinkDto;
 import com.innowise.coffeemachine.entity.Drink;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface DrinkMapper {
-
-    DrinkMapper DRINK_MAPPER = Mappers.getMapper(DrinkMapper.class);
 
     DrinkDto drinkToDrinkDto(Drink drink);
 
--- src/main/java/com/innowise/coffeemachine/mapper/UserMapper.java
+++ src/main/java/com/innowise/coffeemachine/mapper/UserMapper.java
@@ -3,12 +3,9 @@ package com.innowise.coffeemachine.mapper;
 import com.innowise.coffeemachine.dto.UserDto;
 import com.innowise.coffeemachine.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    UserMapper USER_MAPPER = Mappers.getMapper(UserMapper.class);
 
     User userDtoToUser(UserDto userDto);
 
